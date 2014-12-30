fc4bb1e41b7c9139b727d4485de976d413b300ce
Paul-Émile MINY <paulemile.miny@gmail.com>
2014-12-30 09:57:00 +0100

    initial commit

---
+++ editors/js/redactor.js
@@ -0,0 +1,36 @@
+(function($) {
+
+Drupal.wysiwyg.editor.init.redactor = function(settings) {
+}
+
+/**
+ * Attach this editor to a target element.
+ *
+ * See Drupal.wysiwyg.editor.attach.none() for a full desciption of this hook.
+ */
+Drupal.wysiwyg.editor.attach.redactor = function(context, params, settings) {
+  $('#' + params.field).redactor(settings);
+};
+
+/**
+ * Detach a single or all editors.
+ *
+ * See Drupal.wysiwyg.editor.detach.none() for a full desciption of this hook.
+ */
+Drupal.wysiwyg.editor.detach.redactor = function (context, params, trigger) {
+  $('#' + params.field).redactor('core.destroy');
+};
+
+Drupal.wysiwyg.editor.instance.redactor = {
+
+  setContent: function (content) {
+    $('#' + this.field).redactor('code.set', content);
+  },
+
+  getContent: function () {
+    return $('#' + this.field).redactor('code.get');
+  }
+};
+
+})(jQuery);
+
